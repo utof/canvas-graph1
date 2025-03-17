43ee73d42e7a40fa6fbf16a2be99c2df0bc9c08c
utof <bondar030@mail.ru>
2025-03-17 22:17:09 +0300

    init commit, testing stuff out

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+	server: {
+		host: '127.0.0.1',
+		port: 5173
+	}
+});
