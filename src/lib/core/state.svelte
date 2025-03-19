a81c18aee4f8a7763fb7230f2b5062bc07f83d02
utof <bondar030@mail.ru>
2025-03-19 19:34:45 +0300

    app state init

---
+++ src/lib/core/state.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	// TODO why is it in svelte? Is it worse if TS only?
+	import type { Plugin, Command, Note } from './types/index';
+
+	export const app = $state<{
+		plugins: Record<string, Plugin>;
+		commands: Record<string, Command>;
+		activeNoteId: string | null;
+		notes: Record<string, Note>;
+	}>({
+		plugins: {},
+		commands: {},
+		activeNoteId: null,
+		notes: {}
+	}); // TODO why state?
+
+	const activeNote = $derived(app.activeNoteId ? app.notes[app.activeNoteId] : null);
+	export function getActiveNote() {
+		return activeNote;
+	}
+
+	// TODO why have these functions when the methods already exist in the interfaces?
+	export function registerPlugin(plugin: Plugin) {
+		// TODO why not plugin id?
+		app.plugins[plugin.id] = plugin;
+	}
+
+	// TODO function to get plugin ... why?
+
+	export function runCommand(id: string, ...args: any[]) {
+		const command = app.commands[id];
+		if (command) {
+			return command.run(...args);
+		}
+		return null;
+	}
+</script>
